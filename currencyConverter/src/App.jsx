bb8af8c923b12f11269c049312a254c456c498e3
Divyansh <officialdslc1552005@gmail.com>
2025-06-25 23:11:29 +0530

    Updated currecny converter UI

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -44,7 +44,7 @@ function App(){
                        
                     }}
                 >
-                    <div className="w-full mb-1">
+                    <div className="w-full mb-1 text-left">
                         <InputBox
                             label="From"
                             amount={amount}
@@ -64,7 +64,7 @@ function App(){
                         </button>
                     </div>
 
-                  <div className="w-full mt-1 mb-4">
+                  <div className="w-full mt-1 mb-4 text-left">
                         <InputBox
                             label="To"
                             amount={convertedAmount}
@@ -74,7 +74,7 @@ function App(){
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-xl">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -8,10 +8,10 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json()) // becoz api se file json mein nahi , string mein aayegi usko json mein convert krna pdega
         .then((res) => setData(res[currency])) // to hold the data becoz useState 
-        console.log(data);
+        // console.log(data);
     }, [currency]) // currency ki value agar change ho then we wanna call it again so its in dependency array
     
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
