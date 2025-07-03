8462986f9c8c451551763693e19d85100a203bc5
Divyansh <officialdslc1552005@gmail.com>
2025-07-03 14:31:00 +0530

    React-router-done

--- 7ReactRouter/src/Components/User/User.jsx
+++ 7ReactRouter/src/Components/User/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+  const {userid} = useParams()
   return (
-    <div>User</div>
+    <div className='bg-zinc-800 text-white text-2xl  text-center p-4 rounded-md '>User : {userid}</div>
   )
 }
 
--- 7ReactRouter/src/main.jsx
+++ 7ReactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 import User from './Components/User/User.jsx'
+import Github , {githubInfoLoader} from './Components/Github/Github.jsx'
 
  
 
@@ -40,6 +41,9 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader = {githubInfoLoader}
+      path='github' element={<Github />} />
     </Route>
   )
 )
